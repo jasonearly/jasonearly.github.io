1948db5f80cad11ef7ad8165da939444d1f69d9d
Jason Early <theoffice@gruntmonkey.com>
2017-09-04 16:53:44 -0500

    optimizing

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,111 @@
-var gulp = require('gulp'),
-    shell = require('gulp-shell'),
-		minifyHTML = require('gulp-minify-html'),
-		sass = require('gulp-sass'),
-    importCss = require('gulp-import-css'),
-    autoprefixer = require('gulp-autoprefixer'),
-    uncss = require('gulp-uncss'),
-    minifyCss = require('gulp-minify-css'),
-    rename = require('gulp-rename'),
-    glob = require('glob'),
-		imagemin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant'),
-    jpegtran = require('imagemin-jpegtran'),
-    gifsicle = require('imagemin-gifsicle'),
-    optipng = require('imagemin-optipng');
+var config = require('./gulpconfig.json');
+var	gulp = require('gulp');
+var	shell = require('gulp-shell');
+var minifyHTML = require('gulp-minify-html');
+var cloudflare = require('gulp-cloudflare');
+var runSequence = require('run-sequence');
+var autoprefixer = require('gulp-autoprefixer');
+var uncss = require('gulp-uncss');
+var minifyCss = require('gulp-minify-css');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var jpegtran = require('imagemin-jpegtran');
+var gifsicle = require('imagemin-gifsicle');
+var replace = require('gulp-replace');
+var fs = require('fs');
+var download = require('gulp-download');
 
 gulp.task('jekyll', function() {
-  return gulp.src('index.html', { read: false })
-    .pipe(shell([
-      'jekyll build'
-  ]));
+	return gulp.src('index.html', { read: false })
+		.pipe(shell([
+			'jekyll build'
+		]));
 });
 
-gulp.task('html', ['jekyll'], function() {
-    return gulp.src('_site/**/*.html')
-        .pipe(minifyHTML({
-            quotes: true
-        }))
-        .pipe(gulp.dest('_site/'));
+gulp.task('optimize-images', function () {
+	return gulp.src(['_site/**/*.jpg', '_site/**/*.jpeg', '_site/**/*.gif', '_site/**/*.png'])
+		.pipe(imagemin({
+			progressive: false,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant(), jpegtran(), gifsicle()]
+		}))
+		.pipe(gulp.dest('_site/'));
 });
 
-gulp.task('css', ['jekyll'], function() {
-   return gulp.src('css/style.scss')
-       .pipe(sass())
-       .pipe(importCss())
-       .pipe(autoprefixer())
-       .pipe(uncss({
-           html: glob.sync("_site/**/*.html"),
-           ignore: [
-               'label.active',
-               '.dark-mode',
-               'span.tweet-time',
-               /(#|\.)(is-)/,
-               /(#|\.)(has-)/,
-               /(#|\.)(js-)/
-          ]
-       }))
-       .pipe(minifyCss({keepBreaks:false}))
-       .pipe(rename('style.min.css'))
-       .pipe(gulp.dest('_site/style/'));
+gulp.task('optimize-css', function() {
+   return gulp.src('_site/css/main.css')
+	   .pipe(autoprefixer())
+	   .pipe(uncss({
+		   html: ['_site/**/*.html'],
+		   ignore: []
+	   }))
+	   .pipe(minifyCss({keepBreaks: false}))
+	   .pipe(gulp.dest('_site/css/'));
 });
 
-gulp.task('images', ['jekyll'], function () {
-    return gulp.src('images/**')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant(), jpegtran(), optipng(), gifsicle()]
-        }))
-        .pipe(gulp.dest('_site/images'));
+gulp.task('optimize-html', function() {
+	return gulp.src('_site/**/*.html')
+		.pipe(minifyHTML({
+			quotes: true
+		}))
+		.pipe(replace(/<link href=\"\/css\/main.css\"[^>]*>/, function(s) {
+			var style = fs.readFileSync('_site/css/main.css', 'utf8');
+			return '<style>\n' + style + '\n</style>';
+		}))
+		.pipe(gulp.dest('_site/'));
 });
 
-gulp.task('build', [ 'css', 'images', 'html']);
+gulp.task('fetch-newest-analytics', function() {
+	return download('https://www.google-analytics.com/analytics.js')
+    	.pipe(gulp.dest('assets/'));
+});
+
+gulp.task('rsync-files', function() {
+	return gulp.src('index.html', { read: false })
+		.pipe(shell([
+			'cd _site && rsync -az --delete . ' + config.remoteServer + ':' + config.remotePath
+		]));
+});
+
+gulp.task('purge-cache', function() {
+	var options = {
+		token: config.cloudflareToken,
+		email: config.cloudflareEmail,
+		domain: config.cloudflareDomain
+	};
+
+	cloudflare(options);
+});
+
+gulp.task('raw-deploy', function(callback) {
+	runSequence(
+		'jekyll',
+		'rsync-files',
+		'purge-cache',
+		callback
+	);
+});
+
+gulp.task('dry-run', function(callback) {
+	runSequence(
+		'fetch-newest-analytics',
+		'jekyll',
+		'optimize-images',
+		'optimize-css',
+		'optimize-html',
+		callback
+	);
+});
+
+gulp.task('deploy', function(callback) {
+	runSequence(
+		'fetch-newest-analytics',
+		'jekyll',
+		'optimize-images',
+		'optimize-css',
+		'optimize-html',
+		'rsync-files',
+		'purge-cache',
+		callback
+	);
+});
