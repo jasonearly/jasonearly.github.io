fdeea3ea0a11aa16333d7c722f5798f3a328a69d
Jason Early <theoffice@gruntmonkey.com>
2018-05-16 14:24:03 -0500

    added workbox and sw cacheing

--- sw.js
+++ sw.js
@@ -8,40 +8,89 @@ if (workbox) {
   console.log("Boo! Workbox didn't load 😬");
 }
 
-// Offline Analytics
-workbox.googleAnalytics.initialize();
 
-// Cache CSS and JS files
-workbox.routing.registerRoute(
-  /\.(?:js|css)$/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'static-resources',
-  }),
-);
+// install static files to cache
+var CACHE_NAME = 'static-cache';
+var urlsToCache = [
+  '.',
+  'index.html',
+  'styles/main.css'
+];
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
 
-// Cache images
-workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg)$/,
-  workbox.strategies.cacheFirst({
-    cacheName: 'images',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-      }),
-    ],
-  }),
-);
 
-// Cache google fonts
-workbox.routing.registerRoute(
-  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-  workbox.strategies.cacheFirst({
-    cacheName: 'googleapis',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 30,
-      }),
-    ],
-  }),
-);
+
+// fetch static files from cache
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      return response || fetchAndCache(event.request);
+    })
+  );
+});
+
+function fetchAndCache(url) {
+  return fetch(url)
+  .then(function(response) {
+    // Check if we received a valid response
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return caches.open(CACHE_NAME)
+    .then(function(cache) {
+      cache.put(url, response.clone());
+      return response;
+    });
+  })
+  .catch(function(error) {
+    console.log('Request failed:', error);
+    // You could return a custom offline 404 page here
+  });
+}
+
+
+// // Offline Analytics
+// workbox.googleAnalytics.initialize();
+
+// // Cache CSS and JS files
+// workbox.routing.registerRoute(
+//   /\.(?:js|css|html)$/,
+//   workbox.strategies.staleWhileRevalidate({
+//     cacheName: 'static-resources',
+//   }),
+// );
+
+// // Cache images
+// workbox.routing.registerRoute(
+//   /\.(?:png|gif|jpg|jpeg|svg)$/,
+//   workbox.strategies.cacheFirst({
+//     cacheName: 'images',
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxEntries: 60,
+//         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+//       }),
+//     ],
+//   }),
+// );
+
+// // Cache google fonts
+// workbox.routing.registerRoute(
+//   new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+//   workbox.strategies.cacheFirst({
+//     cacheName: 'googleapis',
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxEntries: 30,
+//       }),
+//     ],
+//   }),
+// );
