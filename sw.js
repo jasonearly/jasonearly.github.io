9bccc17cd964deb36f0f2092547d95af2a72254a
Jason Early <theoffice@gruntmonkey.com>
2018-05-16 14:38:44 -0500

    added workbox and sw cacheing

--- sw.js
+++ sw.js
@@ -9,88 +9,90 @@ if (workbox) {
 }
 
 
+// Offline Analytics
+workbox.googleAnalytics.initialize();
+
+// Cache CSS and JS files
+workbox.routing.registerRoute(
+  /\.(?:js|css|html)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'static-resources',
+  }),
+);
+
+// Cache images
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
+
+// Cache google fonts
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'googleapis',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30,
+      }),
+    ],
+  }),
+);
+
+
+
+//exploration
 // install static files to cache
-var CACHE_NAME = 'static-cache';
-var urlsToCache = [
-  '.',
-  'index.html',
-  'styles/main.css'
-];
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+// var CACHE_NAME = 'static-cache';
+// var urlsToCache = [
+//   '.',
+//   'index.html',
+//   'styles/main.css'
+// ];
+// self.addEventListener('install', function(event) {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//     .then(function(cache) {
+//       return cache.addAll(urlsToCache);
+//     })
+//   );
+// });
 
 
 
 // fetch static files from cache
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-    .then(function(response) {
-      return response || fetchAndCache(event.request);
-    })
-  );
-});
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//     .then(function(response) {
+//       return response || fetchAndCache(event.request);
+//     })
+//   );
+// });
 
-function fetchAndCache(url) {
-  return fetch(url)
-  .then(function(response) {
-    // Check if we received a valid response
-    if (!response.ok) {
-      throw Error(response.statusText);
-    }
-    return caches.open(CACHE_NAME)
-    .then(function(cache) {
-      cache.put(url, response.clone());
-      return response;
-    });
-  })
-  .catch(function(error) {
-    console.log('Request failed:', error);
-    // You could return a custom offline 404 page here
-  });
-}
-
-
-// // Offline Analytics
-// workbox.googleAnalytics.initialize();
-
-// // Cache CSS and JS files
-// workbox.routing.registerRoute(
-//   /\.(?:js|css|html)$/,
-//   workbox.strategies.staleWhileRevalidate({
-//     cacheName: 'static-resources',
-//   }),
-// );
-
-// // Cache images
-// workbox.routing.registerRoute(
-//   /\.(?:png|gif|jpg|jpeg|svg)$/,
-//   workbox.strategies.cacheFirst({
-//     cacheName: 'images',
-//     plugins: [
-//       new workbox.expiration.Plugin({
-//         maxEntries: 60,
-//         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-//       }),
-//     ],
-//   }),
-// );
-
-// // Cache google fonts
-// workbox.routing.registerRoute(
-//   new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-//   workbox.strategies.cacheFirst({
-//     cacheName: 'googleapis',
-//     plugins: [
-//       new workbox.expiration.Plugin({
-//         maxEntries: 30,
-//       }),
-//     ],
-//   }),
-// );
+// function fetchAndCache(url) {
+//   return fetch(url)
+//   .then(function(response) {
+//     // Check if we received a valid response
+//     if (!response.ok) {
+//       throw Error(response.statusText);
+//     }
+//     return caches.open(CACHE_NAME)
+//     .then(function(cache) {
+//       cache.put(url, response.clone());
+//       return response;
+//     });
+//   })
+//   .catch(function(error) {
+//     console.log('Request failed:', error);
+//     // You could return a custom offline 404 page here
+//   });
+// }
