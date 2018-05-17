daef5178cb74772634958ce01ff6ce3aa58b397d
Jason Early <theoffice@gruntmonkey.com>
2018-05-17 10:44:29 -0500

    readded workbox and saving to cache on visit

--- sw.js
+++ sw.js
@@ -8,95 +8,55 @@ if (workbox) {
   console.log("Boo! Workbox didn't load 😬");
 }
 
-//
-// workbox.precaching.precacheAndRoute([]);
-//
-//
-//
-// // Offline Analytics
-// workbox.googleAnalytics.initialize();
-//
-// // Cache CSS and JS files
-// workbox.routing.registerRoute(
-//   /\.(?:js|css)$/,
-//   workbox.strategies.staleWhileRevalidate({
-//     cacheName: 'static-resources',
-//   }),
-// );
-//
-// // Cache images
-// workbox.routing.registerRoute(
-//   /\.(?:png|gif|jpg|jpeg|svg)$/,
-//   workbox.strategies.staleWhileRevalidate({
-//     cacheName: 'images',
-//     plugins: [
-//       new workbox.expiration.Plugin({
-//         maxEntries: 60,
-//         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-//       }),
-//     ],
-//   }),
-// );
-//
-// // Cache google fonts
-// workbox.routing.registerRoute(
-//   new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-//   workbox.strategies.staleWhileRevalidate({
-//     cacheName: 'googleapis',
-//     plugins: [
-//       new workbox.expiration.Plugin({
-//         maxEntries: 30,
-//       }),
-//     ],
-//   }),
-// );
+workbox.googleAnalytics.initialize();
+
+workbox.routing.registerRoute(
+  new RegExp('/(.*)'),
+  workbox.strategies.staleWhileRevalidate(),
+);
+
+workbox.routing.registerRoute(
+  // Cache CSS files
+  /.*\.css/,
+  // Use cache but update in the background ASAP
+  workbox.strategies.staleWhileRevalidate({
+    // Use a custom cache name
+    cacheName: 'css-cache',
+  })
+);
+
+workbox.routing.registerRoute(
+  // Cache JS files
+  /.*\.js/,
+  // Use cache but update in the background ASAP
+  workbox.strategies.staleWhileRevalidate({
+    // Use a custom cache name
+    cacheName: 'js-cache',
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'googleapis',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30,
+      }),
+    ],
+  }),
+);
 
 
-
-//exploration
-// install static files to cache
-// var CACHE_NAME = 'static-cache';
-// var urlsToCache = [
-//   '.',
-//   'index.html',
-//   'styles/main.css'
-// ];
-// self.addEventListener('install', function(event) {
-//   event.waitUntil(
-//     caches.open(CACHE_NAME)
-//     .then(function(cache) {
-//       return cache.addAll(urlsToCache);
-//     })
-//   );
-// });
-
-
-
-// fetch static files from cache
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//     .then(function(response) {
-//       return response || fetchAndCache(event.request);
-//     })
-//   );
-// });
-
-// function fetchAndCache(url) {
-//   return fetch(url)
-//   .then(function(response) {
-//     // Check if we received a valid response
-//     if (!response.ok) {
-//       throw Error(response.statusText);
-//     }
-//     return caches.open(CACHE_NAME)
-//     .then(function(cache) {
-//       cache.put(url, response.clone());
-//       return response;
-//     });
-//   })
-//   .catch(function(error) {
-//     console.log('Request failed:', error);
-//     // You could return a custom offline 404 page here
-//   });
-// }
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
