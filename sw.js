7094272edaa4f605ff38cc1de66dc91cf1a9b2fb
Jason Early <theoffice@gruntmonkey.com>
2019-02-10 17:32:54 -0600

    loaded sw fetch response actions

--- sw.js
+++ sw.js
@@ -1,69 +1,116 @@
 console.log('Hello from sw.js');
+// fresh scripts from scratch
+addEventListener('install', function (event){
+  console.log('The service worker is installing...');
+});
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
+addEventListener('activate', function (event){
+  console.log('The service worker is activated.');
+});
 
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-} else {
-  console.log("Boo! Workbox didn't load 😬");
-}
+addEventListener('fetch', function (fetchEvent){
+  // console.log('The service worker is listening.');
+  const request = fetchEvent.request;
 
-workbox.googleAnalytics.initialize();
+  fetchEvent.respondWith(
+    fetch(request)
+    .then(responseFromFetch =>{
+      return responseFromFetch;
+    }) //end fetch then
 
-workbox.precaching.precacheAndRoute([]);
-
-workbox.routing.registerRoute(
-  new RegExp('/(.*)'),
-  workbox.strategies.staleWhileRevalidate(),
-);
-
-workbox.routing.registerRoute(
-  new RegExp('/work/img/(.*)'),
-  workbox.strategies.staleWhileRevalidate(),
-);
-
-workbox.routing.registerRoute(
-  // Cache CSS files
-  /.*\.css/,
-  // Use cache but update in the background ASAP
-  workbox.strategies.staleWhileRevalidate({
-    // Use a custom cache name
-    cacheName: 'css-cache',
-  })
-);
-
-workbox.routing.registerRoute(
-  // Cache JS files
-  /.*\.js/,
-  // Use cache but update in the background ASAP
-  workbox.strategies.staleWhileRevalidate({
-    // Use a custom cache name
-    cacheName: 'js-cache',
-  })
-);
-
-workbox.routing.registerRoute(
-  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'googleapis',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 30,
-      }),
-    ],
-  }),
-);
+    // if offline/no network connection show this
+    .catch(error => {
+      return new Response('<h1>Oops!</h1> <p>Something went wrong.</p>',
+      {
+        headers: {'Content-type' : 'text/html; charset=utf-8'}
+      }
+    );
+  }) // end feth catch
+); // end respondWith
+}); // addEventListener
 
 
-workbox.routing.registerRoute(
-  /\.(?:png|gif|jpg|jpeg|svg)$/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'images',
-    plugins: [
-      new workbox.expiration.Plugin({
-        maxEntries: 60,
-        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-      }),
-    ],
-  }),
-);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// Scripts and recipies from Workbox
+//
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
+//
+// if (workbox) {
+//   console.log(`Yay! Workbox is loaded 🎉`);
+// } else {
+//   console.log("Boo! Workbox didn't load 😬");
+// }
+//
+// workbox.googleAnalytics.initialize();
+//
+// workbox.precaching.precacheAndRoute([]);
+//
+// workbox.routing.registerRoute(
+//   new RegExp('/(.*)'),
+//   workbox.strategies.staleWhileRevalidate(),
+// );
+//
+// workbox.routing.registerRoute(
+//   new RegExp('/work/img/(.*)'),
+//   workbox.strategies.staleWhileRevalidate(),
+// );
+//
+// workbox.routing.registerRoute(
+//   // Cache CSS files
+//   /.*\.css/,
+//   // Use cache but update in the background ASAP
+//   workbox.strategies.staleWhileRevalidate({
+//     // Use a custom cache name
+//     cacheName: 'css-cache',
+//   })
+// );
+//
+// workbox.routing.registerRoute(
+//   // Cache JS files
+//   /.*\.js/,
+//   // Use cache but update in the background ASAP
+//   workbox.strategies.staleWhileRevalidate({
+//     // Use a custom cache name
+//     cacheName: 'js-cache',
+//   })
+// );
+//
+// workbox.routing.registerRoute(
+//   new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+//   workbox.strategies.staleWhileRevalidate({
+//     cacheName: 'googleapis',
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxEntries: 30,
+//       }),
+//     ],
+//   }),
+// );
+//
+//
+// workbox.routing.registerRoute(
+//   /\.(?:png|gif|jpg|jpeg|svg)$/,
+//   workbox.strategies.staleWhileRevalidate({
+//     cacheName: 'images',
+//     plugins: [
+//       new workbox.expiration.Plugin({
+//         maxEntries: 60,
+//         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+//       }),
+//     ],
+//   }),
+// );
