fe5cbaaa07b4465fafb81cb9e56858f8999c4458
Jason Early <theoffice@gruntmonkey.com>
2019-02-11 11:20:06 -0600

    creating and adding to cache, plus cache > network fetch

--- sw.js
+++ sw.js
@@ -1,22 +1,61 @@
+const staticCacheName = 'staticFiles';
+
+
 console.log('Hello from sw.js');
 // fresh scripts from scratch
-addEventListener('install', function (event){
+
+// install and add to cache
+addEventListener('install', function (installEvent){
   console.log('The service worker is installing...');
-});
+  installEvent.waitUntil(
+    caches.open(staticCacheName)
+    .then(staticCache => {
+      //nice to haves
+       staticCache.addAll([
+        // static files to cache
+        // '/path/font.woff',
+        // '/path/icon.svg',
+      ]); //end return addAll
+
+      // must haves
+      return staticCache.addAll([
+        // static files to cache
+        // '/path/styles.css',
+        // '/path/js/app.js',
+      ]); //end return addAll
+
+    }) //end then
+  ); //end waitUntil
+}); //end addEventListener
+
+
 
 addEventListener('activate', function (event){
   console.log('The service worker is activated.');
 });
+
+
 
 addEventListener('fetch', function (fetchEvent){
   // console.log('The service worker is listening.');
   const request = fetchEvent.request;
 
   fetchEvent.respondWith(
-    fetch(request)
-    .then(responseFromFetch =>{
-      return responseFromFetch;
-    }) //end fetch then
+    // first look in cache
+    caches.match(request)
+    .then(responseFromCache => {
+      if (responseFromCache) {
+        return responseFromCache;
+      } // end if
+      // otherwise fetch from network
+      return fetch(request);
+    }) // end match then
+
+
+    // .then(responseFromFetch =>{
+    //   return responseFromFetch;
+    // }) //end fetch then
+
 
     // if offline/no network connection show this
     .catch(error => {
@@ -26,8 +65,9 @@ addEventListener('fetch', function (fetchEvent){
       }
     );
   }) // end feth catch
+
 ); // end respondWith
-}); // addEventListener
+}); // end addEventListener
 
 
 
